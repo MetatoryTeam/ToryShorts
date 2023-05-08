aa5ee2ed119cbe2700c8eafe23b086a6e78f3fb1
ika9810 <ika7204@naver.com>
2023-05-08 17:29:30 +0900

    ✨[UPD]Add music & update system

--- generateToryShorts.py
+++ generateToryShorts.py
@@ -3,62 +3,73 @@ import random
 from moviepy.editor import *
 
 
-
-# /intro 디렉토리에서 intro.mov 선택
-intro_file = "./intro/Intro.mov"
+# Load intro video and audio
+intro_dir = "./intro"
+intro_file = os.path.join(intro_dir, "Intro.mov")
 intro = VideoFileClip(intro_file)
 
-intro_dir = "./intro"
 only_music_file = "only.mp3"
-intro_music = AudioFileClip(os.path.join(intro_dir, only_music_file))
-intro.set_audio(intro_music)
+intro_music_file = os.path.join(intro_dir, only_music_file)
+intro_music = AudioFileClip(intro_music_file)
+intro = intro.set_audio(intro_music)
 
-# /outro 디렉토리에서 outro.mov 선택
-outro_file = "./outro/Outro.mp4"
+
+# Load outro video
+outro_dir = "./outro"
+outro_file = os.path.join(outro_dir, "Outro.mp4")
 outro = VideoFileClip(outro_file)
 
 
-# /music 디렉토리에서 무작위로 음악 파일 선택
-music_dir = "./music"
-music_files = [f for f in os.listdir(music_dir) if f.endswith(('.mp3', '.wav'))]
-random_music_file = random.choice(music_files)
-
-# /asset 디렉토리에서 무작위로 영상 파일 선택
+# Load random video file and trim to 9 seconds
 asset_dir = "./asset"
 video_files = [f for f in os.listdir(asset_dir) if f.endswith(('.mp4', '.avi', '.mov', '.mkv'))]
 random_video_file = random.choice(video_files)
 
-# 선택한 영상 파일을 읽고,9초로 자르기
-video = VideoFileClip(os.path.join(asset_dir, random_video_file))
-video_duration = video.duration
+video_file = os.path.join(asset_dir, random_video_file)
+video = VideoFileClip(video_file)
 
-if video_duration > 9:
-    start_time = random.uniform(0, video_duration - 9)
+if video.duration > 9:
+    start_time = random.uniform(0, video.duration - 9)
     end_time = start_time + 9
     video = video.subclip(start_time, end_time)
 
-# /image 디렉토리에서 무작위로 이미지 파일 선택
+# Load random image file and resize to 1280x1280
+name_dir = "./"
+name_image_file = "name.png"
+
+name_file = os.path.join(name_dir, name_image_file)
+image_name = ImageClip(name_file).set_pos((0, 2909))
+
+
+# Load random image file and resize to 1280x1280
 image_dir = "./image"
 image_files = [f for f in os.listdir(image_dir) if f.endswith(('.jpg', '.jpeg', '.png'))]
 random_image_file = random.choice(image_files)
 
-#선택한 이미지 파일을 읽고, 1280*1280 정사각형으로 resize 하고, 영상 중앙에 위치하도록 조정
-image = ImageClip(os.path.join(image_dir, random_image_file)).resize((1280, 1280)).set_pos((440, 401))
+image_file = os.path.join(image_dir, random_image_file)
+image = ImageClip(os.path.join(image_dir, random_image_file)).resize((1000, 1000)).set_pos((580, 302))
 
-# 선택한 음악 파일을 읽기
-music = AudioFileClip(os.path.join(music_dir, random_music_file))
 
-# 음악과 영상, 이미지를 합치기
-video_with_music_and_image = CompositeVideoClip([video, image.set_duration(video_duration)])
-final_clip = concatenate_videoclips([intro, video_with_music_and_image, outro])
+# Load random music file
+music_dir = "./music"
+music_files = [f for f in os.listdir(music_dir) if f.endswith(('.mp3', '.wav'))]
+random_music_file = random.choice(music_files)
 
-# 음악의 길이를 영상의 길이에 맞게 자르거나 반복
+music_file = os.path.join(music_dir, random_music_file)
+music = AudioFileClip(music_file)
+
+
+# Combine video, image and music
+video_with_image = CompositeVideoClip([video, image.set_duration(video.duration), image_name.set_duration(video.duration)])
+final_clip = concatenate_videoclips([intro, video_with_image, outro])
+
 if music.duration < final_clip.duration:
-    music = music.loop_to(final_clip.duration)
+    music = music.loop_to_duration(final_clip.duration)
 else:
     music = music.subclip(0, final_clip.duration)
-    
+
 final_clip = final_clip.set_audio(music)
 
-# 합쳐진 동영상 파일을 내보내기
+
+# Export final video
 final_clip.write_videofile("output_video.mp4", codec="libx264", audio_codec="aac")
